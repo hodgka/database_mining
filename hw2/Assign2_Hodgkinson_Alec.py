c97bd93b1b94c2f303433869ccb6251866a68aa6
Alec Hodgkinson <Hodgka@rpi.edu>
2016-09-27 01:18:34 -0400

    part 2 works

--- hw2/Assign2_Hodgkinson_Alec.py
+++ hw2/Assign2_Hodgkinson_Alec.py
@@ -9,6 +9,8 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
+from collections import Counter
 import sys
 import re
 from sklearn.decomposition import PCA, KernelPCA
@@ -46,17 +48,14 @@ def create_kernel(data, kfunc=None, spread=1):
         K = np.dot(data, data.T)
     else:
         K = np.exp(np)
+
+    n = np.shape(data)[0]
+    A = np.eye(n) - np.ones((n, n))/n
+    K = np.dot(A, np.dot(K.T, A))
+
     return K
 
-
-def center_kernel(K):
-    n = np.shape(K)[0]
-    A = np.eye(n) - np.ones((n, n))/n
-    K_centered = np.dot(A, np.dot(K.T, A))
-    return K_centered
-
-
-def normalize_kernel(K):
+# def normalize_kernel(K):
     w = np.diag(1./np.sqrt(np.diag(K)))
     K_normalized = np.dot(w, np.dot(K.T, w))
     return K_normalized
@@ -66,6 +65,8 @@ def dominant_eigenvectors(K, n=0, alpha=None):
     eigen_val, eigen_vec = np.linalg.eigh(K)
     eigen_val = eigen_val[::-1]
     eigen_vec = eigen_vec[:, ::-1]
+    lambda_i = eigen_val/float(np.shape(K)[0])
+    # TODO need to scale eigen vectors and finish KPCA
     # eigen_vec = np.nan_to_num(np.divide(eigen_vec, inv_sqrt_lambda))
     if n != 0:
         # if n is specified, return n dominant dimensions
@@ -82,7 +83,7 @@ def dominant_eigenvectors(K, n=0, alpha=None):
         print(eigen_vec)
         return (eigen_val[:i], eigen_vec[:, :i])
     else:
-        print("Alpha is None")
+        return(eigen_val[:], eigen_vec[:, :])
 
 
 def reduce_data_dimensionality(K, C):
@@ -114,6 +115,62 @@ def dominant_pca_eigenvectors(S, n=0, alpha=None):
     else:
         print("Alpha is None")
 
+
+def epmf(n=100000, d=10):
+
+    half_diagonals = np.array(list(itertools.product([1, -1], repeat=d)))
+    # randomly choose 2n half diagonals
+    diag_inds = np.random.choice(np.shape(half_diagonals)[0], 2*n)
+    # make tuples of the n pairs of diagonals
+    diags = half_diagonals[diag_inds, :]
+    diags = zip(*[iter(diags)]*2)
+    # calculate the angle
+
+    def angle(x1, x2):
+        return np.degrees(np.arccos(np.dot(x1, x2) /
+                          (np.linalg.norm(x1) * np.linalg.norm(x2))))
+
+    angles = [angle(vec[0], vec[1]) for vec in diags]
+
+    C = Counter(angles)
+    total = sum(C.values())
+    for key, value in C.items():
+        C[key] = value / float(total)
+    plt.bar(C.keys(), C.values(), color='g')
+    plt.show()
+
+    keys = np.array(list(C.keys()))
+    min_key = np.min(keys)
+    max_key = np.max(keys)
+    value_range = max_key - min_key
+    mean = np.mean(angles)
+    variance = np.var(angles)
+
+    return (min_key, max_key, value_range, mean, variance)
+
+
+
+def test():
+
+    ###########
+    #  part 1 #
+    ###########
+
+    kernel_data = np.array([[540, 162, 2.5], [540, 162, 2.5], [332.5, 228, 0]])
+    # K = create_kernel(kernel_data, 'linear')
+    # K = center_kernel(K)
+    # eigen_vals, eigen_vecs = dominant_eigenvectors(K)
+    # print(eigen_vals)
+    # print(eigen_vecs)
+    # print(K)
+
+    ###########
+    #  part 2 #
+    ###########
+    # print(epmf())
+    # print(epmf(d=100))
+    # print(epmf(d=1000))
+
 ##########################################################################
 ##########################################################################
 
@@ -125,27 +182,28 @@ if __name__ == "__main__":
         data = np.array(list(map(float, re.split(r'[,\n]', d)[:-1])))
         data = data.reshape((-1, 9))
 
-    K = create_kernel(data, "linear")
-    K_normalized = normalize_kernel(K)
-    C = dominant_eigenvectors(K_normalized, n=2, alpha=0.95)[1]
-    # print(C)
-    A = reduce_data_dimensionality(K, C)
-    print(np.shape(K))
-    print((C))
-    # for i in range(9):
-    #     for i in range():
-    #         plt.scatter(data[:, i], data[:, j])
-    # plt.scatter(data[:, 8], data[:, 7])
-    fig = plt.figure()
-    plt.subplot(121)
-    plt.scatter(A[0], A[1])
+    test()
+    # K = create_kernel(data, "linear")
+    # K_normalized = normalize_kernel(K)
+    # C = dominant_eigenvectors(K_normalized, n=2, alpha=0.95)[1]
+    # # print(C)
+    # A = reduce_data_dimensionality(K, C)
+    # print(np.shape(K))
+    # print((C))
+    # # for i in range(9):
+    # #     for i in range():
+    # #         plt.scatter(data[:, i], data[:, j])
+    # # plt.scatter(data[:, 8], data[:, 7])
+    # fig = plt.figure()
+    # plt.subplot(121)
+    # plt.scatter(A[0], A[1])
+    # # plt.show()
+    # # variance = float(sys.argv[2])
+    # S = create_covariance(data)
+    # C_pca = dominant_pca_eigenvectors(S, 2)[1]
+    # A_pca = np.dot(data, C_pca)
+    # print(np.shape(A_pca))
+    # # print(C_pca)
+    # plt.subplot(122)
+    # plt.scatter(A_pca[:, 0], A_pca[:, 1])
     # plt.show()
-    # variance = float(sys.argv[2])
-    S = create_covariance(data)
-    C_pca = dominant_pca_eigenvectors(S, 2)[1]
-    A_pca = np.dot(data, C_pca)
-    print(np.shape(A_pca))
-    # print(C_pca)
-    plt.subplot(122)
-    plt.scatter(A_pca[:, 0], A_pca[:, 1])
-    plt.show()
