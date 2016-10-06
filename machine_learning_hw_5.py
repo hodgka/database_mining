248e59256b0971fe664f86514a98c6e0a595b3b6
Alec Hodgkinson <Hodgka@rpi.edu>
2016-10-06 11:23:29 -0400

    bias works, need variance

--- machine_learning_hw_5.py
+++ machine_learning_hw_5.py
@@ -2,7 +2,8 @@ import numpy as np
 import numpy.random as random
 import matplotlib.pyplot as plt
 
-# np.set_printoptions(precision=6, linewidth=80, threshold=np.inf, suppress=True)
+# np.set_printoptions(precision=6, linewidth=80, threshold=np.inf,
+#                     suppress=True)
 np.set_printoptions(precision=6, linewidth=80, suppress=True)
 
 
@@ -50,19 +51,28 @@ def generate_points(N=100000):
 
 
 def calculate_g_weights(dataset):
+    '''
+    get weights for g = ax +b, between two datapoints for a dataset
+    '''
     a = (dataset[:, 3] - dataset[:, 1])/(dataset[:, 2] - dataset[:, 0])
     b = dataset[:, 1] + a*dataset[:, 0]
     return np.array([a, b]).T
 
 
 def g_avg(weights):
+    '''
+    calculate average set of weights given all weights for g
+    '''
     return np.average(weights, axis=0)
 
 
 def bias(g, x):
-    N = np.shape(x)[0]
-    x = np.concatenate((x[:, 0], x[:, 2]), axis=0)
+    '''
+    calculate the total bias for Eout
+    '''
 
+    x = np.concatenate((x[:, 0], x[:, 2]), axis=0)
+    N = np.shape(x)[0]
     # g[0] is a, g[1] is b
     # multiply g[1] by vector of ones so that you can add them together
     g_bar_values = g[0]*x + g[1]*np.ones(np.shape(x))
@@ -70,44 +80,47 @@ def bias(g, x):
     return np.sum(g_minus_f_squared)/N
 
 
-# def bias(dataset, g_weights):
-#     N = np.shape(g_weights)[0]
-#     d = np.shape(dataset)[0]
-#     g_error = np.empty((N, d))
-#     for j in range(d):
-#         for i in range(N):
-#             # print(g_weights[i][0])
-#             # print(g_weights[i][0], g_weights[i][1], dataset[j])
-#             g_error[i][j] = (g_weights[i][0] * dataset[j] +
-#                              g_weights[i][1] - dataset[j]**2)**2
-#     g_bias = (1/(d*N)) * np.sum(g_error)
-#     print(g_bias)
-#     # g_data_range = g(dataset, g_weights)
-#     # print(g_data_range)
-
-
 def var(g, x):
-    N = np.shape(x)[0]
+    '''
+    calculate variance for Eout
+    '''
     x = np.concatenate((x[:, 0], x[:, 2]), axis=0)
-    g_bar_squared = g[0]*x + g[1]*np.ones(np.shape(x))**2
+    N = np.shape(x)[0]
+    g_bar_squared = np.average(g[0]*x + g[1]*np.ones(np.shape(x))**2)
     print(g_bar_squared)
     return
 
 if __name__ == "__main__":
-    trials = 50
-    dataset = generate_points()
-    data_range = np.linspace(-1, 1, 50)
+    trials = 10
+    points = 100000
+    # create a dataset of 2N points in [-1,1]
+    # dataset = generate_points(points)
+    # calculate the weights of each g on the dataset
+    # g_weights = calculate_g_weights(dataset)
+    # get the average weights for all the g on the dataset
+    # average_hypothesis = g_avg(g_weights)
+    data_points = np.empty((points*trials, 4))
+    weights = np.empty((points*trials, 2))
+    print(np.shape(weights))
+    for i in range(trials):
+        dataset = generate_points(points)
+        data_points[i*points: (i+1)*points, :] = dataset
+        weights[i*points: (i+1)*points, :] = calculate_g_weights(dataset)
+    g_bar = g_avg(weights)
 
-    g_weights = calculate_g_weights(dataset)
-    average_hypothesis = g_avg(g_weights)
-    fig = plt.figure()
-    plt.subplot(111)
-    x = np.linspace(-1, 1, 500)
-    y1 = average_hypothesis[0]*x + average_hypothesis[1] * np.ones(np.shape(x))
-    y2 = x**2
-    plt.plot(x, y1, 'r')
-    plt.plot(x, y2, 'b')
-    plt.show()
+    bias_value = bias(g_bar, dataset)
+    var_value = var(g_bar, dataset)
+    print(bias_value)
+    print(var_value)
+    # fig = plt.figure()
+    # plt.subplot(111)
+    # x = np.linspace(-1, 1, 500)
+    # y1 = average_hypothesis[0]*x + average_hypothesis[1] * np.ones(np.shape(x))
+    # y2 = x**2
+    # plt.plot(x, y1, 'r', label="g_bar")
+    # plt.plot(x, y2, 'b', label="f(x)")
+    # plt.legend()
+    # plt.show()
     # sample_bias = bias(average_hypothesis, dataset)
     # sample_var = var(average_hypothesis, dataset)
     # bias(data_range, g_weights)
@@ -117,4 +130,4 @@ if __name__ == "__main__":
     #     data = generate_points()
     #     g_weights = calculate_g_weights(data)
     #     bias(data_range, g_weights)
-        # print(generate_points()[:50, :])
+    #     # print(generate_points()[:50, :])
