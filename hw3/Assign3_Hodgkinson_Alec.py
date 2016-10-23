a1edb98a17c6598ca32274fc618a41d981c46c26
Alec Hodgkinson <Hodgka@rpi.edu>
2016-10-22 23:52:11 -0400

    'hw3'

--- hw3/Assign3_Hodgkinson_Alec.py
+++ hw3/Assign3_Hodgkinson_Alec.py
@@ -36,7 +36,9 @@ def compute_K(data, t=None):
     if t is None or t.lower() == "linear":
         K = np.dot(x, x.T)
     elif t.lower() == 'quadratic':
-        K = np.linalg.matrix_power(np.dot(x, x.T), 2)
+        # K = np.linalg.matrix_power(np.dot(x, x.T), 2)
+        xxt = np.dot(x, x.T)
+        K = np.dot(xxt, xxt.T)
 
     else:
         K = gaussian_kernel(x, spread)
@@ -67,42 +69,42 @@ def SMO(K, data, C, eps):
     x = data[:, :-1]
     y = data[:, -1]
 
-    alpha = np.zeros(np.shape(x)[0])
-    alpha_prev = np.ones(np.shape(alpha))
+    a = np.zeros(np.shape(x)[0])
+    a_prev = np.ones(np.shape(a))
     tol = 10**-5
     tryall = True
-    while np.linalg.norm(alpha-alpha_prev) > eps:
+    while np.linalg.norm(a-a_prev) > eps:
         it += 1
         print("Running iteration: {0}".format(it))
 
-        alpha_prev = np.copy(alpha)
+        a_prev = np.copy(a)
         for j in range(n):
-            if tryall is False and (alpha[j]-tol < 0 or alpha[j]+tol > C):
+            if tryall is False and (a[j]-tol < 0 or a[j]+tol > C):
                 continue
             for i in np.random.random_integers(0, n-1, n):
                 if i == j:
                     continue
-                if tryall is False and (alpha[i]-tol < 0 or alpha[i]+tol > C):
+                if tryall is False and (a[i]-tol < 0 or a[i]+tol > C):
                     continue
                 kij = K[i, i] + K[j, j] - 2*K[i, j]
                 if kij == 0:
                     continue
-                alpha_i = alpha[i]
-                alpha_j = alpha[j]
-                L, H = compute_L_H(y[i], y[j], C, alpha_i, alpha_j)
+                a_i = a[i]
+                a_j = a[j]
+                L, H = compute_L_H(y[i], y[j], C, a_i, a_j)
                 if L == H:
                     continue
-                alpha[j] = alpha_j + y[j]*compute_E_ij(alpha, y, K, i, j)/kij
-                if alpha[j] < L:
-                    alpha[j] = L
-                elif alpha[j] > H:
-                    alpha[j] = H
-                # print(alpha_i, y[i], y[j], alpha_j, alpha[j])
-                alpha[i] = alpha_i + y[i]*y[j]*(alpha_j - alpha[j])
+                a[j] = a_j + y[j]*compute_E_ij(a, y, K, i, j)/kij
+                if a[j] < L:
+                    a[j] = L
+                elif a[j] > H:
+                    a[j] = H
+                # print(a_i, y[i], y[j], a_j, a[j])
+                a[i] = a_i + y[i]*y[j]*(a_j - a[j])
         if tryall:
             tryall = False
-    alpha_filtered = alpha[(tol < alpha) & (alpha < C-tol)]
-    return (alpha, alpha_filtered)
+    a_filtered = a[(tol < a) & (a < C-tol)]
+    return (a, a_filtered)
 
 
 def compute_L_H(yi, yj, C, ai, aj):
@@ -115,66 +117,39 @@ def compute_L_H(yi, yj, C, ai, aj):
     return (L, H)
 
 
-def compute_E_ij(alpha, y, K, i, j):
-    ei1 = 0
-    ej1 = 0
-    alpha[alpha < 0] = 0
-    for p in range(len(y)):
-        ei1 += alpha[p]*y[p]*K[p, i]
-        ej1 += alpha[p]*y[p]*K[p, j]
-    ei1 -= y[i]
-    ej1 -= y[j]
-
-    # ei2 = 0
-    # ei2 = np.sum(alpha*y*K[:, i])
-    # ei2 -= y[i]
-    # ej2 = 0
-    # ej2 = np.sum(alpha*y*K[:, j])
-    # ej2 -= y[j]
-    # ei = 0
-    # ej = 0
-    # for l in range(len(y)):
-    #     ei += alpha[l]*y[l]*K[l, i]
-    #     ei += alpha[l]*y[l]*K[l, j]
-    # print(ei - ej)
+def compute_E_ij(a, y, K, i, j):
+    ei = np.sum(a*y*K[:, i]) - y[i]
+    ej = np.sum(a*y*K[:, j]) - y[j]
     return ei1-ej1
 
 
-def get_support_vectors(data, alpha):
-    alpha[alpha < 0] = 0
-    indices = np.array([i for i in range(len(alpha)) if alpha[i] != 0])
+def get_support_vectors(data, a):
+    a[a < 0] = 0
+    indices = np.array([i for i in range(len(a)) if a[i] != 0])
     svs = data[indices]
 
     return svs
 
 
-def compute_bias(data, alpha, K, C, tol):
-    y = data[:, -1]
-    y[alpha <= 0] = 0
-    alpha[alpha <= 0] = 0
-    n = np.count_nonzero(alpha)
-    b = np.empty(np.shape(y))
-    for i in range(len(y)):
-        b[i] = y[i] - np.sum(alpha*y*K[:, i])
-    b_avg = np.average(b)
-    return b_avg
+def compute_bias(y, a, K, C, tol):
+    ytemp = np.copy(y)
+    ytemp[a <= 0] = 0
+    b = ytemp - np.sum(alpha*ytemp*K[:, :], axis=0)
+    return np.mean(b)
 
 
-def compute_accuracy(data, K, alpha, b, C, tol):
-    x = data[:, :-1]
-    y = data[:, -1]
-    alpha[alpha < 0] = 0
-    alpha[alpha > C-tol] = 0
-    y[alpha == 0] = 0
-    error = 0
-    yhat = np.empty(np.shape(y))
-    for j in range(len(y)):
-        temp = 0
-        for i in range(len(alpha)):
-            temp += alpha[i]*y[i] * K[i, j]
-            # print(alpha[i], y[i], K[i, 0])
-        temp += b
-        yhat[j] = np.sign(temp)
+def compute_accuracy(y, K, a, b, C, tol):
+    y_temp = np.copy(y)
+    y_temp[a <= 0] = 0
+    y_temp[a > C-tol] = 0
+    yhat = np.sign(np.sum(a*y_temp*K[:, :], axis=0) + b)
+    # for j in range(len(y)):
+    #     temp = 0
+    #     for i in range(len(a)):
+    #         temp += a[i]*y[i] * K[i, j]
+    #         # print(a[i], y[i], K[i, 0])
+    #     temp += b
+    #     yhat[j] = np.sign(temp)
 
     accuracy = np.sum([1 for k in range(len(y))
                        if y[k] != yhat[k]])/len(y)
@@ -191,16 +166,16 @@ if __name__ == "__main__":
     x = data[:, :-1]
     y = data[:, -1]
     # print(SMO(K, data, C, eps))
-    alpha, alpha_f = SMO(K, data, C, eps)
-    svs = get_support_vectors(data, alpha_f)
-    b = compute_bias(data, alpha, K, C, 10**-5)
-    acc = compute_accuracy(data, K, alpha, b, C, 10**-5)
+    a, a_f = SMO(K, data, C, eps)
+    svs = get_support_vectors(data, a_f)
+    b = compute_bias(data, a, K, C, 10**-5)
+    acc = compute_accuracy(data, K, a, b, C, 10**-5)
     if args.w is not None:
         with open("Assign3_Hodgkinson_Alec.txt", 'w') as f:
             f.write("Kernel type: {0}\n Support Vectors: {1}\n Bias: {2}\n\
                     Accuracy {3}".format(args.kernel_type, svs, b, acc))
     print("Kernel Type: {0}")
-    print("Support Vector Lagrange Multipliers: ", alpha_f)
+    print("Support Vector Lagrange Multipliers: ", a_f)
     print("Support vectors: \n", svs)
     print("Bis: ", b)
     print("Accuracy: ", acc)
